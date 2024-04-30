01c8cb46b407b0696a438b86824f4b541e351084
Kim Tae Won <33194980+ktw1525@users.noreply.github.com>
2024-04-30 17:27:52 +0900

    v0.1
    1. 3상 누설전류 데이터를 생성
    2. 학습
    3. 검증
    프로그램 제작

--- dataLoader.py
+++ dataLoader.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 class MakeInputDatas:
@@ -8,7 +9,14 @@ class MakeInputDatas:
         self.Vp = 220
         self.w = 2 * np.pi / Period
         self.n_t = np.arange(1, Len + 1)
+        self.regen()
 
+    def add_noise(self, sig, rate):
+        rms = np.sqrt(np.mean(sig**2))
+        noise = (2 * np.random.rand(len(sig)) - 1) * rms * rate
+        return sig + noise
+
+    def regen(self):
         # Coefficients initialization
         self.G1_t = 0.0002 * np.random.rand()
         self.C1_t = 0.0002 * np.random.rand()
@@ -33,28 +41,29 @@ class MakeInputDatas:
                    self.current(self.V3_t, self.dV3dn_t, self.G3_t, self.C3_t)
         self.I_t = self.add_noise(self.I_t, 0.00)
 
-    def add_noise(self, sig, rate):
-        rms = np.sqrt(np.mean(sig**2))
-        noise = (2 * np.random.rand(len(sig)) - 1) * rms * rate
-        return sig + noise
-
     def current(self, v, dvdn, g, c):
         return g * v + c * dvdn
 
     def get_data(self):
-        return {
-            "n": self.n_t,
-            "V1": self.V1_t,
-            "V2": self.V2_t,
-            "V3": self.V3_t,
-            "dV1dn": self.dV1dn_t,
-            "dV2dn": self.dV2dn_t,
-            "dV3dn": self.dV3dn_t,
-            "I": self.I_t,
-            "G1": self.G1_t,
-            "G2": self.G2_t,
-            "G3": self.G3_t,
-            "C1": self.C1_t,
-            "C2": self.C2_t,
-            "C3": self.C3_t
-        }
+        data = []
+        data.append(torch.tensor([
+            self.V1_t,
+            self.V2_t,
+            self.V3_t,
+            self.dV1dn_t,
+            self.dV2dn_t,
+            self.dV3dn_t,
+            self.I_t
+        ]).float().flatten())
+        data = torch.stack(data)
+        return data
+    
+    def get_target(self):
+        return torch.tensor(torch.tensor([
+            self.G1_t,
+            self.G2_t,
+            self.G3_t,
+            self.C1_t,
+            self.C2_t,
+            self.C3_t,
+        ]).float().flatten())
--- valid.py
+++ valid.py
@@ -16,7 +16,7 @@ INPUT_SIZE = DATALEN * WAVEFORMS_INPUT
 WAVEFORMS_VECTORS = WAVEFORMS_INPUT*2
 MATELEMENTS_INPUT = (WAVEFORMS_INPUT**2)*2
 MATELEMENTS = (WAVEFORMS_INPUT**2)
-BATCHSIZE = 1000
+BATCHSIZE = 1
 OUTPUT_SIZE = 6         # G1, G2, G3, C1, C2, C3
 
 dataPipe = dlr.MakeInputDatas(DATALEN, LEN_PER_ONECYCLE);
@@ -34,7 +34,7 @@ class MRM3PNet(nn.Module):
     def __init__(self):
         super(MRM3PNet, self).__init__()
         self.ly1 = nn.Linear(INPUT_SIZE, INPUT_SIZE*2)
-        self.ly2 = nn.Conv1d(1, WAVEFORMS_VECTORS, kernel_size=LEN_PER_ONECYCLE, stride=1, padding=0)
+        self.ly2 = nn.Conv1d(BATCHSIZE, WAVEFORMS_VECTORS, kernel_size=LEN_PER_ONECYCLE, stride=1, padding=0)
         self.ly3 = nn.Linear(INPUT_SIZE*2-LEN_PER_ONECYCLE+1, WAVEFORMS_VECTORS) # WAVEFORMS_VECTORS, MATELEMENTS_INPUT
         self.ly4 = MultiplyLayer()
         self.ly5 = nn.Linear(WAVEFORMS_VECTORS*WAVEFORMS_VECTORS, OUTPUT_SIZE)
@@ -57,31 +57,18 @@ else:
     print("No existing model found, starting training from scratch.")
 
 # 손실 함수 및 최적화 알고리즘 설정
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-def train(model, data_loader, optimizer, criterion, epochs, model_path):
-    index=0
-    while(1):
-        data_loader.regen()
-        index=index+1
-        for epoch in range(epochs):
-            inputs = data_loader.get_data()
-            targets = data_loader.get_target()
-            outputs = model(inputs)
 
-            loss = criterion(outputs, targets)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            print(f'Index [ {index} ], Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
-            if (loss.item()<0.001):
-                break
+def valid(model, data_loader):
+    data_loader.regen()
+    inputs = data_loader.get_data()
+    targets = data_loader.get_target()
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+    print(f'True values: {targets}')
+    print(f'Output values: {outputs}')
+    print(f'loss: {loss}')
 
-try:
-    train(model, dataPipe, optimizer, criterion, EPOCHS, model_path)
-except KeyboardInterrupt:
-    torch.save(model.state_dict(), model_path)
-    print(f'Model saved to {model_path}')
+valid(model, dataPipe)
